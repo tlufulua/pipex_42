ac0ca52d0427dbd44b7ce53d37ca2c82389e9de9
Teresa bulungu Lufuluabo pastor <tlufulua@c3r1s3.42madrid.com>
2022-10-30 19:58:27 +0100

    Practicing file handling

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: tlufulua <tlufulua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 11:48:50 by tlufulua          #+#    #+#             */
-/*   Updated: 2022/10/30 11:50:22 by tlufulua         ###   ########.fr       */
+/*   Updated: 2022/10/30 19:57:40 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,6 @@
 # include <stdlib.h>
 # include <libft.h>
 # include <sys/wait.h>
+# include <fcntl.h>
+# include <stdio.h>
 #endif
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,18 +6,45 @@
 /*   By: tlufulua <tlufulua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 11:46:43 by tlufulua          #+#    #+#             */
-/*   Updated: 2022/10/30 13:54:36 by tlufulua         ###   ########.fr       */
+/*   Updated: 2022/10/30 19:57:28 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
-#include <fcntl.h>
 
-int	main()
+int	main(int argc, char **argv)
 {
-	int x;
+	int 	fd;
+	int 	fd_copy;
+	char	buffer[100];
+	char	buffercp[100];
 
-	x = open("../text1.txt", O_RDONLY);	
-	ft_printf("x: %d\n", x);
+	fd = open(argv[1], O_RDWR);	
+	if (argc == 2)
+	{
+		ft_printf("fd: %d\n", fd);
+		read(fd, buffer, 50);
+		ft_printf("buffer: %s\n", buffer);
+		fd = close(fd);
+		ft_printf("close: %d\n", fd);
+	}
+	else if (argc == 3)
+	{
+		ft_printf("fd: %d\n", fd);
+		write(fd, argv[2], ft_strlen(argv[2]));
+		close(fd);
+		fd = open(argv[1], O_RDONLY);
+		fd_copy = 5; 
+		dup2(fd, fd_copy);
+		read(fd, buffer, 50);
+		ft_printf("buffer: %s\n", buffer);
+		fd = close(fd);
+		ft_printf("close fd: %d\n", fd);
+		ft_printf("fd_copy: %i\n", fd_copy);
+		read(fd_copy, buffercp, 50);
+		ft_printf("buffercp: %s\n", buffercp);
+		fd_copy = close(fd_copy);
+		ft_printf("close fd_copy: %d\n", fd);
+	}
 	return (0);
 }
