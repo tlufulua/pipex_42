a3df36f3e9b0e7abda5dce519bca80db556c82e8
tlufulua <tlufulua@student.42madrid.com>
2022-10-31 23:29:13 +0100

    Basic execve

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,13 +6,31 @@
 /*   By: tlufulua <tlufulua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 11:46:43 by tlufulua          #+#    #+#             */
-/*   Updated: 2022/10/31 19:29:11 by tlufulua         ###   ########.fr       */
+/*   Updated: 2022/10/31 23:18:27 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
 
-int	main(int argc, char **argv)
+//Con este main, al ejecutar ./pipex ls, hará lo mismo que si ponemos ls en la
+//terminal.
+
+void	hola(char *path, char **argv, char **env)
 {
+	execve(path, argv, env);
+	//No escribe "Hola" porque execve ejecuta el proceso al que llama acabando
+	//el proceso en el que está antes
+	write(1, "Hola\n", 5);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	char	*path;
+
+	argc = 0;
+	path = "/bin/ls";
+	argv++;
+	hola(path, argv, env);
+	write(1, "Adiós\n", 6);
 	return (0);
 }
