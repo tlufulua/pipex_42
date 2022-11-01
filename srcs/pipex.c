02eb43d53f6a3e2415d1600c1bc141377ce77a70
tlufulua <tlufulua@student.42madrid.com>
2022-11-01 02:06:59 +0100

    more practice

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,32 +6,42 @@
 /*   By: tlufulua <tlufulua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 11:46:43 by tlufulua          #+#    #+#             */
-/*   Updated: 2022/11/01 01:38:11 by tlufulua         ###   ########.fr       */
+/*   Updated: 2022/11/01 02:06:09 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
 
-//Con este main, al ejecutar ./pipex ls, hará lo mismo que si ponemos ls en la
+//Con este main, al ejecutar ./pipex wc, hará lo mismo que si ponemos wc en la
 //terminal.
 
-void	duplicate(char *argv)
+void	salida(char **argv)
 {
-	int	fd;
+	int	fdout;
 
-	fd = open(argv, O_WRONLY | O_CREAT, 0777);
-	dup2(fd, 1);
+	fdout = open(*argv, O_WRONLY | O_CREAT, 0777);
+	dup2(fdout, 1);
+	*(argv) = NULL; //elimino el último argumento ya que va a ser el
+					//archivo donde se guarda el output.
 }
 
+void	entrada(char *argv)
+{
+	int	fdin;
+
+	fdin = open(argv, O_RDONLY);
+	dup2(fdin, 0);
+}
 int	main(int argc, char **argv, char **env)
 {
 	char	*path;
 
-	duplicate(argv[argc - 1]); //va a redireccionar la salida a un archivo (es por practicar)
-	path = "/bin/ls";
-	argv[argc - 1] = NULL; //elimino el último argumento ya que va a ser el
-						   //archivo donde se guarda el output.
-	argv += 1;
+	salida(&argv[argc - 1]); //va a redireccionar la salida a un archivo 
+							//(es por practicar)
+	entrada(argv[1]); //execve va a usar la información dentro del archivo
+					  //que está como segundo parámetro.
+	path = "/usr/bin/wc";
+	argv += 2;
 	execve(path, argv, env);
 	//No escribe "Hola" porque execve ejecuta el proceso al que llama acabando
 	//el proceso en el que está antes
