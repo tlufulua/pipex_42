97b345acdf2d5c5d1c98d777fb0eacc5c7883d62
tlufulua <tlufulua@student.42madrid.com>
2022-11-01 01:12:47 +0100

    working...

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: tlufulua <tlufulua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 11:46:43 by tlufulua          #+#    #+#             */
-/*   Updated: 2022/10/31 23:18:27 by tlufulua         ###   ########.fr       */
+/*   Updated: 2022/11/01 01:11:56 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,16 @@
 //Con este main, al ejecutar ./pipex ls, hará lo mismo que si ponemos ls en la
 //terminal.
 
-void	hola(char *path, char **argv, char **env)
-{
-	execve(path, argv, env);
-	//No escribe "Hola" porque execve ejecuta el proceso al que llama acabando
-	//el proceso en el que está antes
-	write(1, "Hola\n", 5);
-}
-
 int	main(int argc, char **argv, char **env)
 {
 	char	*path;
 
 	argc = 0;
 	path = "/bin/ls";
-	argv++;
-	hola(path, argv, env);
-	write(1, "Adiós\n", 6);
+	argv += 1;
+	execve(path, argv, env);
+	//No escribe "Hola" porque execve ejecuta el proceso al que llama acabando
+	//el proceso en el que está antes
+	write(1, "Hola\n", 5);
 	return (0);
 }
