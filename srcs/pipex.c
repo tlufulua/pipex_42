23bfbcd1fa3e26570b6f91c7d71a7e42c0599b3e
tlufulua <tlufulua@student.42madrid.com>
2022-11-02 01:12:35 +0100

    ready to check leaks and norm

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: tlufulua <tlufulua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 11:46:43 by tlufulua          #+#    #+#             */
-/*   Updated: 2022/11/02 00:35:11 by tlufulua         ###   ########.fr       */
+/*   Updated: 2022/11/02 01:11:56 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ void	child(int *fd, char **argv, char **env)
 	infile = open(argv[1], O_RDONLY, 0777);
 	if (infile == -1)
 		ft_error("\033[31merror:\033[0m can´t open the infile");
-	//dup redirecciona una vez que tengo la entrada (infile) y la salida
-	//(fd[1]) abiertas.
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(infile, STDIN_FILENO);
 	close(fd[0]);
 	vec = ft_split(argv[2], ' ');
-	cmd = get_cmd(vec[0], env); //liberar
+	cmd = get_cmd(vec[0], env);
 	execve(cmd, vec, env);
+	free(cmd);
+	free_split(vec);
 }
 
 void	father(int *fd, char **argv, char **env)
@@ -44,13 +44,16 @@ void	father(int *fd, char **argv, char **env)
 	dup2(outfile, STDOUT_FILENO);
 	close(fd[1]);
 	vec = ft_split(argv[3], ' ');
-	cmd = get_cmd(vec[0], env); //liberar
+	cmd = get_cmd(vec[0], env);
 	execve(cmd, vec, env);
+	free(cmd);
+	free_split(vec);
 }
-/*El programa debe hacer lo mismo que el comando: 
- < archivo1 comando1 | comando2 > archivo2 
- pero ejecuta:
- ./pipex archivo1 "comando1" "comando2" archivo2*/
+
+void	salida()
+{
+	system("leaks pipex");
+}
 
 int	main(int argc, char **argv, char **env)
 {
@@ -58,6 +61,7 @@ int	main(int argc, char **argv, char **env)
 	pid_t	check;
 	pid_t	fr;
 
+	//atexit(salida);
 	if (argc == 5)
 	{
 		check_arg(argv);
@@ -65,12 +69,12 @@ int	main(int argc, char **argv, char **env)
 		fr = fork();
 		if (fr == -1)
 			ft_error("\033[31merror\033[0m: fork failure");
-		if (fr == 0) //hijo
+		if (fr == 0)
 			child(fd, argv, env);
 		check = waitpid(fr, NULL, 0);
 		if (!check)
 			return (-1);
-		if (fr) //padre
+		if (fr)
 			father(fd, argv, env);
 	}
 	else
